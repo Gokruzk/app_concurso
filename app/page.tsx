62fb0c6c904d54e0c728672b62a581131f514dcd
Sarkosky <sivelez96@gmail.com>
2024-06-27 10:15:48 -0500

    register

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-  import Link from "next/link";
+import Link from "next/link";
+import LinkButton from "@/components/LInkButton";
 
 
 export default function Home() {
@@ -7,9 +8,8 @@ export default function Home() {
     <main className="bg-gray-50 dark:bg-gray-900">
       
       <div>
-      <button className=" m-5 bg-red-500 sm:bg-orange-400 text-center hover:bg-red-200 py-5 px-8 font-bold text-white rounded">
-            ¡EMPEZAR!
-        </button>
+        <LinkButton href="/login"  style="m-5 bg-red-500 sm:bg-orange-400 text-center hover:bg-red-200 py-5 px-8 font-bold text-white rounded" title="Login"/>
+    
         <br />
         <button className=" m-5 bg-red-500 sm:bg-orange-400 text-center hover:bg-red-200 py-5 px-8 font-bold text-white rounded">
             ACERCA DE
